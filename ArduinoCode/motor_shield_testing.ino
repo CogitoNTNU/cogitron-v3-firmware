d6d671bc15748731c4f229d38a34a1ef5eed18ac
EdvardSKT <114014282+EdvardSKT@users.noreply.github.com>
2022-11-08 19:05:18 +0100

    Update motor_shield_testing.ino

--- ArduinoCode/motor_shield_testing.ino
+++ ArduinoCode/motor_shield_testing.ino
@@ -5,16 +5,38 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *myMotor3 = AFMS.getMotor(3);
+Adafruit_DCMotor *myMotor4 = AFMS.getMotor(4);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   AFMS.begin();
-  myMotor->setSpeed(30);
-  myMotor->run(FORWARD);
+  myMotor3->setSpeed(100);
+  myMotor3->run(BACKWARD);
+  myMotor4->setSpeed(100);
+  myMotor4->run(BACKWARD);
 }
 
 void loop() {
     // put your setup code here, to run once:
+  if(Serial.available()){
+    String input = Serial.readStringUntil('\n');
+    int speed = input.toInt();
+    if(speed == 1){
+      myMotor3->run(FORWARD);
+      myMotor4->run(FORWARD);
+    }
+    else if(speed == 2){
+      myMotor3->run(BACKWARD);
+      myMotor4->run(BACKWARD);
+    }
+    else if(speed == 3){
+      myMotor3->run(RELEASE);
+      myMotor4->run(RELEASE);
+    }
+  }
+
+  
 
 }
